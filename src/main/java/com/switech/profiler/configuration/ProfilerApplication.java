82b2983e5c23afefeb3e88d0a27d700945da98bb
Julien <julien1105@outlook.fr>
2019-02-21 18:39:55 +0100

    v1.0.0.0 initial commit

---
+++ src/main/java/com/switech/profiler/configuration/ProfilerApplication.java
@@ -0,0 +1,12 @@
+package com.switech.profiler.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilerApplication.class, args);
+	}
+}
